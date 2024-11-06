3842973b2e22e2e19193142ef0f132cc1b24937a
rahawaeh_113 <rahawehjen@gmail.com>
2024-11-06 10:57:35 +0700

    fix id and else

--- main_tracking/resources/views/beranda.blade.php
+++ main_tracking/resources/views/beranda.blade.php
@@ -7,6 +7,7 @@
     <title>Beranda</title>
     <meta name="description" content="">
     <meta name="keywords" content="">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
     @include('style.gaya')
 
@@ -31,15 +32,16 @@
             <div class="container-md position-relative" data-aos="fade-up" data-aos-delay="100">
                 <div class="row gy-5 justify-content-between">
                     <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                        <h2><span>Selamat Datang Di </span><span class="accent">TrackMate</span></h2>
-                        <p>TrackMate adalah aplikasi pelacakan barang yang dirancang untuk menjadi mitra Anda dalam
-                            mengawasi setiap langkah perjalanan barang. TrackMate memastikan Anda selalu mendapatkan
-                            informasi terbaru tentang status pengiriman barang Anda. Dengan TrackMate, Anda dapat dengan
-                            mudah mengelola pengiriman Anda, sehingga tidak ada lagi kekhawatiran tentang kehilangan
-                            atau keterlambatan barang.</p>
+                        <h2><span>Selamat Datang di </span><span class="accent">APL Tracker</span></h2>
+                            <p>APL Tracker adalah aplikasi pelacakan yang dirancang khusus untuk membantu jurusan APL
+                                dalam memantau dan mengelola berbagai aktivitas pelacakan. Aplikasi ini memastikan bahwa
+                                Anda selalu mendapatkan informasi terbaru tentang status dan perkembangan setiap
+                                kegiatan yang dipantau. Dengan APL Tracker, pengelolaan pelacakan menjadi lebih mudah
+                                dan efisien, sehingga semua proses dapat berlangsung dengan lancar dan terstruktur.</p>
+
                     </div>
                     <div class="col-lg-5 order-1 order-lg-2">
-                        <img src="{{ asset ('template/assets/img/hero-img.svg') }}" class="img-fluid" alt="">
+                        <img src="{{ asset('template/assets/img/hero-img.svg') }}" class="img-fluid" alt="">
                     </div>
                 </div>
             </div>
@@ -65,14 +67,14 @@
                         <div class="col-xl-3 col-md-6">
                             <div class="icon-box">
                                 <div class="icon"><i class="bi bi-geo-alt"></i></div>
-                                <h4 class="title">Sesuai Alamat</h4>
+                                <h4 class="title">Dikirim Sesuai Detail Alamat</h4>
                             </div>
 
                         </div><!--End Icon Box -->
                         <div class="col-xl-3 col-md-6">
                             <div class="icon-box">
-                                <div class="icon"><i class="bi bi-geo-alt"></i></div>
-                                <h4 class="title">isi sendiri</h4>
+                                <div class="icon"><i class="fas fa-money-bill-wave"></i></div>
+                                <h4 class="title">Layanan Ekonomis</h4>
                             </div>
                         </div><!--End Icon Box -->
                     </div>
