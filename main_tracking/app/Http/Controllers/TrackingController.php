e192fd12b232d10909db9d0f5d2dcdf5b84cf841
Nawall07 <nawalramadhan2007@gmail.com>
2024-11-10 12:40:27 +0700

    p

--- main_tracking/app/Http/Controllers/TrackingController.php
+++ main_tracking/app/Http/Controllers/TrackingController.php
@@ -7,86 +7,86 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 
-// class TrackingController extends Controller
-// {
-//     public function dasboard() {
-//         return view('page.beranda');
-//     }
+class TrackingController extends Controller
+{
+    public function dasboard() {
+        return view('page.beranda');
+    }
 
 
-//     public function about() {
-//         return view('page.tentang');
-//     }
+    public function about() {
+        return view('page.tentang');
+    }
 
-//     public function view(Tracking $tracking)
-//     {    
-//         return view('page.cek.isi_barang', compact('tracking'));
-//     }
+    public function view(Tracking $tracking)
+    {    
+        return view('page.cek.isi_barang', compact('tracking'));
+    }
 
 
-//     public function index()
-//     {
-//         $trackings = Tracking::all();
-//         return view('page.data_barang', compact('trackings'));
-//     }
+    public function index()
+    {
+        $trackings = Tracking::all();
+        return view('page.data_barang', compact('trackings'));
+    }
 
-//     public function create()
-//     {
-//         return view('page.kirim.create');
-//     }
+    public function create()
+    {
+        return view('page.kirim.create');
+    }
 
-//     public function store(Request $request)
-//     {
-//         $validated = $request->validate([
-//             'nama_pengirim' => 'required|string',
-//             'no_hp_pengirim' => 'required|string',
-//             'alamat_pengirim' => 'required|string',
-//             'nama_penerima' => 'required|string',
-//             'no_hp_penerima' => 'required|string',
-//             'alamat_penerima' => 'required|string',
-//             'nama_barang' => 'required|string',
-//             'jumlah_barang' => 'required|integer',
-//             'jenis_pengiriman' => 'required|in:reguler,cepat',
-//             'pesan_pengirim' => 'nullable|string',
-//         ]);
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nama_pengirim' => 'required|string',
+            'no_hp_pengirim' => 'required|string',
+            'alamat_pengirim' => 'required|string',
+            'nama_penerima' => 'required|string',
+            'no_hp_penerima' => 'required|string',
+            'alamat_penerima' => 'required|string',
+            'nama_barang' => 'required|string',
+            'jumlah_barang' => 'required|integer',
+            'jenis_pengiriman' => 'required|in:reguler,cepat',
+            'pesan_pengirim' => 'nullable|string',
+        ]);
 
-//         $validated['biaya_pengiriman'] = $request->jenis_pengiriman === 'reguler' ? 10000 : 20000;
+        $validated['biaya_pengiriman'] = $request->jenis_pengiriman === 'reguler' ? 10000 : 20000;
 
-//         Tracking::create($validated);
+        Tracking::create($validated);
 
-//         return redirect()->route('tampil');
-//     }
+        return redirect()->route('tampil');
+    }
 
-//     public function edit(Tracking $tracking)
-//     {
-//         return view('page.cek.edit', compact('tracking'));
-//     }
+    public function edit(Tracking $tracking)
+    {
+        return view('page.cek.edit', compact('tracking'));
+    }
 
-//     public function update(Request $request, Tracking $tracking)
-//     {
-//         $validated = $request->validate([
-//             'nama_pengirim' => 'required|string',
-//             'no_hp_pengirim' => 'required|string',
-//             'alamat_pengirim' => 'required|string',
-//             'nama_penerima' => 'required|string',
-//             'no_hp_penerima' => 'required|string',
-//             'alamat_penerima' => 'required|string',
-//             'nama_barang' => 'required|string',
-//             'jumlah_barang' => 'required|integer',
-//             'jenis_pengiriman' => 'required|in:reguler,cepat',
-//             'pesan_pengirim' => 'nullable|string',
-//         ]);
+    public function update(Request $request, Tracking $tracking)
+    {
+        $validated = $request->validate([
+            'nama_pengirim' => 'required|string',
+            'no_hp_pengirim' => 'required|string',
+            'alamat_pengirim' => 'required|string',
+            'nama_penerima' => 'required|string',
+            'no_hp_penerima' => 'required|string',
+            'alamat_penerima' => 'required|string',
+            'nama_barang' => 'required|string',
+            'jumlah_barang' => 'required|integer',
+            'jenis_pengiriman' => 'required|in:reguler,cepat',
+            'pesan_pengirim' => 'nullable|string',
+        ]);
 
-//         $validated['biaya_pengiriman'] = $request->jenis_pengiriman === 'reguler' ? 10000 : 20000;
+        $validated['biaya_pengiriman'] = $request->jenis_pengiriman === 'reguler' ? 10000 : 20000;
 
-//         $tracking->update($validated);
+        $tracking->update($validated);
 
-//         return redirect()->route('tampil');
-//     }
+        return redirect()->route('tampil');
+    }
 
-//     public function destroy(Tracking $tracking)
-//     {
-//         $tracking->delete();
-//         return redirect()->route('tampil');
-//     }
-// }
+    public function destroy(Tracking $tracking)
+    {
+        $tracking->delete();
+        return redirect()->route('tampil');
+    }
+}
