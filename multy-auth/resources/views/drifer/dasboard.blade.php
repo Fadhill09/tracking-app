66c5171b42977ab3c865f739614d1f468ee6679e
rahawaeh_113 <rahawehjen@gmail.com>
2024-11-23 09:24:21 +0700

    merge

--- multy-auth/resources/views/drifer/dasboard.blade.php
+++ multy-auth/resources/views/drifer/dasboard.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-<<<<<<< HEAD
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
@@ -37,29 +36,10 @@
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
-=======
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="author" content="Untree.co">
-    <link rel="shortcut icon" href="{{ asset('fury/favicon.png') }}">
-
-    <meta name="description" content="" />
-    <meta name="keywords" content="bootstrap, bootstrap4" />
-
-    <!-- Bootstrap CSS -->
-    <!-- Bootstrap CSS -->
-    <link href="{{ asset('fury/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link href="{{ asset('fury/css/tiny-slider.css') }}" rel="stylesheet">
-    <link href="{{ asset('fury/css/style.css') }}" rel="stylesheet">
-
-    <title>Furni Free Bootstrap 5 Template for Furniture and Interior Design Websites by Untree.co </title>
->>>>>>> eb793347823b5b86acba81d647922616438cb54c
 </head>
 
 <body>
 
-<<<<<<< HEAD
   <!-- ======= Header ======= -->
     @include('drifer.operasi.navbar')<!-- End Header -->
 
@@ -87,23 +67,6 @@
 
   <!-- Template Main JS File -->
   <script src="{{ asset('NiceAdmin/assets/js/main.js') }}"></script>
-=======
-@include('drifer.operasi.header')
-
-@include('drifer.operasi.hero')
-
-@include('drifer.operasi.testimoni')
-
-@include('drifer.operasi.footer')
-
-
-
-
-
-    <script src="{{ asset('fury/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('fury/js/tiny-slider.js') }}"></script>
-    <script src="{{ asset('fury/js/custom.js') }}"></script>
->>>>>>> eb793347823b5b86acba81d647922616438cb54c
 
 </body>
 
